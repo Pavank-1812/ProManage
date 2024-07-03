5bf2efe3d05dc00a8edc3606b3f9e43f9fcf28fa
Pavank-1812 <148217455+Pavank-1812@users.noreply.github.com>
2024-07-04 00:42:05 +0530

    added backend url

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,44 @@
-const express = require("express");
-require("dotenv").config();
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
 const app = express();
-const cors = require("cors");
-const connectDB = require("./Config/db");
+const cors = require('cors');
+const connectDB = require('./Config/db');
+const userRoutes = require('./Routes/userRoutes');
+const taskRoutes = require('./Routes/taskRoutes');
+
 const port = process.env.PORT || 5000;
-const userRoutes = require("./Routes/userRoutes");
-const taskRoutes = require("./Routes/taskRoutes");
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json("Welcome to the Server!");
+app.get('/', (req, res) => {
+  res.json('Welcome to the Server!');
 });
 
-app.get("/health", (req, res) => {
+app.get('/health', (req, res) => {
   res.json({
-    service: "Pro Manage App Server",
-    status: "Active",
+    service: 'Pro Manage App Server',
+    status: 'Active',
     time: new Date(),
   });
 });
 
-app.use("/auth", userRoutes);
-app.use("/task", taskRoutes);
+app.use('/auth', userRoutes);
+app.use('/task', taskRoutes);
+
+
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.listen(port, (err) => {
   if (!err) {
-    console.log("Server is Up & is Running on the port: ", port);
+    console.log('Server is Up & is Running on the port: ', port);
   } else {
-    console.log("Error while connecting to server");
+    console.log('Error while connecting to server');
   }
 });
